2019f94e29d45077969949f0d5615fa58ad0df1d
codedevhulk <132328724+codedevhulk@users.noreply.github.com>
2023-05-02 19:32:09 +0530

    ok

--- src/main/java/com/example/demo/DemoController.java
+++ src/main/java/com/example/demo/DemoController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class DemoController {
 	@GetMapping("/hello")
 	public String hello() {
-		return "hello world!";
+		return "hello world!!!";
 	}
 }
 
